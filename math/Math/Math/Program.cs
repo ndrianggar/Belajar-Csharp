b117f0f9723382fbab0196007ef2477d624090e4
Hendri <ndrianggar@gmail.com>
2021-12-17 10:52:21 +0700

    Math

---
+++ math/Math/Math/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace Math
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MathF.Max(4,9));
+            Console.WriteLine(MathF.Sqrt(90));
+            Console.WriteLine(MathF.Abs(90));
+            Console.WriteLine(MathF.Round(90));
+        }
+    }
+}
