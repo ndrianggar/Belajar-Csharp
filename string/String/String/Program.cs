486b80220bc10cf96943e589305c1ef9ffcf72f1
Hendri <ndrianggar@gmail.com>
2021-12-17 14:40:32 +0700

    string&bool

---
+++ string/String/String/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Hendri ";
+            string lastName = "Angggara";
+            string txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+
+            Console.WriteLine("The length of the txt string is: " + txt.Length);
+           
+            //Method
+            Console.WriteLine(txt.ToUpper());   // Outputs "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+            Console.WriteLine(txt.ToLower());   // Outputs "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+            // String Concatenation
+            string name = firstName + lastName;
+            Console.WriteLine(name);
+
+
+            // Concat
+            string namapertama = "John ";
+            string namakedua = "Doe";
+            string nama = string.Concat(namapertama, namakedua);
+            Console.WriteLine(nama);
+
+            //bool
+            Console.WriteLine(name == nama); // returns True, because the value of x is equal to 10
+
+
+        }
+    }
+}
