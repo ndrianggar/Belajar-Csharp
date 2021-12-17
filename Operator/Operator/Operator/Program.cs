258a263cb250a9971ce325f75b372c84238ae95b
Hendri <ndrianggar@gmail.com>
2021-12-17 10:38:48 +0700

    operator

---
+++ Operator/Operator/Operator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Operator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 5;
+            int y = 6;
+
+            Console.WriteLine("operator pertambahan " +
+                x + y);
+            Console.WriteLine("operator perkalian " +
+    x * y);
+
+            Console.WriteLine("operator pembagian  " +
+  x / y);
+            Console.WriteLine("operator modulus " +
+  x %  y);
+
+            Console.WriteLine("operator increment " +
+  x--);
+        }
+    }
+}
