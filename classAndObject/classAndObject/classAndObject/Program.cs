1c73ee8ec76002fac1907e441b74dbba171b8050
Hendri <ndrianggar@gmail.com>
2021-12-21 10:06:59 +0700

    class and object, access field in class

---
+++ classAndObject/classAndObject/classAndObject/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace classAndObject
+{
+    class Car
+    {
+        public string color = "red"; // cara agar bisa diakses dari luar kelas
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Car myObj = new Car();
+
+            Console.WriteLine(myObj.color);              
+        }
+
+    
+    }
+}
