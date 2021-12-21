687684f06bf539e601e5e39a414f821e97f36cdd
Hendri <ndrianggar@gmail.com>
2021-12-21 11:28:17 +0700

    constructor

---
+++ constructor/constructor/constructor/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace constructor
+{
+    class Program
+    {
+        public string Start;
+        public Program()
+        {
+            Start = "lets run"; // Set the initial value for model
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            // buat objek baru
+            Program myObj = new Program(); // Create an object of the Car Class (this will call the constructor)
+
+            Console.WriteLine(myObj.Start); // Print the value of model
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
