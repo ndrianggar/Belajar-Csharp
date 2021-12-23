568e40ea0187c63bd877e49fd1393b4f84d509a0
Hendri <ndrianggar@gmail.com>
2021-12-23 09:33:43 +0700

    File

---
+++ file/File/File/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+
+/*
+ The File class has many useful methods for creating and getting information about files. For example:
+
+Method      	Description
+===========================================================
+AppendText()	Appends text at the end of an existing file
+Copy()	        Copies a file
+Create()       	Creates or overwrites a file
+Delete()	    Deletes a file
+Exists()	    Tests whether the file exists
+ReadAllText()	Reads the contents of a file
+Replace()   	Replaces the contents of a file with the contents of another file
+WriteAllText()	Creates a new file and writes the contents to it. If the file already exists, it will be overwritten.
+        
+ 
+ 
+ */
+namespace File
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("File Text");
+
+      
+        }
+    }
+
+    class Test
+    {
+        public static void Mains()
+        {
+            string writeText = "test"; // Create a Text String
+             System.IO.File.WriteAllText("filename.txt", writeText);
+
+
+            string readText = System.IO.File.ReadAllText("filename.txt");  // Read the contents of the file
+            Console.WriteLine(readText);  // Output the content
+        }
+    }
+}
