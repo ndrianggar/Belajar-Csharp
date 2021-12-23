75708cf8212bb1a0a45c7577bae38ce519da627e
Hendri <ndrianggar@gmail.com>
2021-12-23 14:35:48 +0700

    Get Api Countries

---
+++ API/RestApi/RestAPI/RestAPI/Controllers/CountriesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RestAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountriesController : ControllerBase
+    {
+        private List<Country> countries = new List<Country>() {
+        new Country(){IdCountry = 0, NameCountry="Indonesian", Language = "Indonesia" },
+        new Country(){IdCountry=0, NameCountry="Saudi Arabia", Language ="Arabic"}
+      };
+        [HttpGet]
+        public IEnumerable<Country> GET()
+        {
+            return countries;
+        }
+    }
+}
---
+++ API/RestApi/RestAPI/RestAPI/Models/Country.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestAPI.Models
+{
+    public class Country
+    {
+        public int IdCountry { get; set; }
+
+        public string NameCountry { get; set; }
+
+        public string Language { get; set; }
+
+
+    }
+}
