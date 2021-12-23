38b0e551ad9eb84f0bb807fb18d358fb738cb552
Hendri <ndrianggar@gmail.com>
2021-12-23 08:35:47 +0700

    Enum

---
+++ enum/Enum/Enum/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Enum
+{
+    enum University
+    {
+       Lipia,
+       UIM,
+       Al_Azhar
+    }
+
+    enum Months
+    {
+        January,
+            February,
+            Maret,
+            April,
+            May,
+            June,
+            July,
+            Agustus,
+            September,
+            Oktober,
+          //  November=2, diberikan nilai default
+          November,
+            Desember
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Penggunaan Enum");
+            University myVar = University.Lipia;
+            Console.WriteLine(myVar);
+
+            int myMonts = (int) Months.Desember;
+            Console.WriteLine(myMonts);
+        }
+    }
+}
